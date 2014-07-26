b6afc7deb4fabee97a1ca27bda9fbc9f4face199
iamgabeortiz <gabeortiz@icloud.com>
2014-07-26 14:57:17 -0400

    Set npp alias to Notepad++

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,4 @@
 Push-Location (Split-Path -parent $profile)
 "components","functions","aliases","extra" | Where-Object {Test-Path "$_.ps1"} | ForEach-Object -process {Invoke-Expression ". .\$_.ps1"}
 Pop-Location
+Set-Alias npp "C:\Program Files (x86)\Notepad++\notepad++.exe"
